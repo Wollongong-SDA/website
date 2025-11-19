8114d8642798e3a951f309f0b38f944546687e3f
Neeron Bhatta <33238007+NeuronButter@users.noreply.github.com>
2025-11-19 21:52:38 +1100

    chore: remove ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
-import node from '@astrojs/node';
 import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [
     vue(),
     UnoCSS({
       injectReset: true
     })
   ],
-  adapter: node({
-    mode: 'standalone'
-  }),
   image: {
     domains: ['10.2.100.6:8055', 'cms.illawarraadventist.org']
   }

65e25450af7700d31826906cdff08465c5732311
Neeron Bhatta <33238007+NeuronButter@users.noreply.github.com>
2025-11-19 20:45:59 +1100

    fix: ts error

--- uno.config.ts
+++ uno.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
       collections: {
         mdi: () => import('@iconify-json/mdi/icons.json').then(i => i.default),
       },
-    }),
+    }) as any,
     presetWind4()
   ],
   theme: {

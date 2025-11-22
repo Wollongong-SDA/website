8829cf5c959003f9631c3d02b88d1bf57ed9e5f0
Neeron Bhatta <33238007+NeuronButter@users.noreply.github.com>
2025-11-22 11:23:15 +1100

    feat: cf image transforms

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
-export const API_URL = 'https://cms.illawarraadventist.org'
+export const API_URL = import.meta.env.CMS_BASE
+export const TRANSFORMATION = `${API_URL}/cdn-cgi/image/format=avif`
 export const ASSET_URL = (path: string) => `${API_URL}/assets/${path}`
+export const IMAGE_URL = (path: string) => `${TRANSFORMATION}/assets/${path}`

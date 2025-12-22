41049169e79c9b22e72319d400b36206b9bce924
Neeron Bhatta <33238007+NeuronButter@users.noreply.github.com>
2025-12-22 22:47:07 +1100

    fix: correct site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import playformCompress from '@playform/compress'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://illawarraadventist.org',
+  site: 'https://www.illawarraadventist.org',
   output: 'static',
   integrations: [
     UnoCSS({
